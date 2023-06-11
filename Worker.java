ba1bb00e9c73522223870d87ce33c583d57a72f3
Elena Karpovich <elekarpovi@tut.by>
2023-06-11 22:08:45 +0300

    Added Program

---
+++ Worker.java
@@ -0,0 +1,17 @@
+public class Worker extends Employee {
+
+    public Worker(String name, String surname, int age, double salary) {
+        super(name, surname, age, salary);
+    }
+
+    @Override
+    public double calculateSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s; Рабочий; %d лет;  ЗП в месяц: %.2f (руб.)",
+                surname, name, age, calculateSalary());
+    }
+}
